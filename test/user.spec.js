6dcd4d8eddab414368560503680f2bf15c26b2c2
Alanna Bouloy <lanihope@gmail.com>
2026-08-11 13:54:04 -0400

    SWIFT-6 built out framework for user endpoint unit tests

--- test/user.spec.js
+++ test/user.spec.js
@@ -5,6 +5,30 @@ const User = db.User;
 const { expect } = require('chai');
 const helpers = require('./test-helpers');
 const supertest = require('supertest');
+
+describe('Users endpoints', () => {
+    const testUsers = helpers.makeUsersArray();
+    const testUser = testUsers[0];
+
+    before('clean the table', () => db.query('TRUNCATE TABLE "Users" RESTART IDENTITY CASCADE'));
+    afterEach('clean the table', ()=> db.query('TRUNCATE TABLE "Users" RESTART IDENTITY CASCADE'));
+
+    describe('/users endpoint', () => { //need to decide if this path should require an admin level access. if so then need to test that as well
+        //Unhappy Path
+        it('returns 404 when no users in table', () => {});
+        //Happy Path
+        it('returns 200 and list of users in table', () => {});
+    });
+
+    describe('/users:userId', ()=> {
+        //Unhappy Path
+         it('returns 401 unauthorized if request lacks proper permissions', () => {});
+         it('returns 404 if no such user exists', () => {});
+        //Happy Path
+        it('returns 200 and user', () => {});
+    })
+
+})
 /* 
 TODO
 Happy Path
